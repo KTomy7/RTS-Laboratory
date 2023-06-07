34ff36aab8dfc68fdee6137fef3e09f562b49f2d
KTomy7 <kisstomi7.kt@gmail.com>
2023-06-07 13:24:09 +0300

    Finshed RTS Labs

--- src/LabExam/Q2/ExecutionThread.java
+++ src/LabExam/Q2/ExecutionThread.java
@@ -5,72 +5,80 @@ import java.util.concurrent.Semaphore;
 
 public class ExecutionThread extends Thread {
     Semaphore semaphore1;
-    private Semaphore semaphore2;
-    private final CountDownLatch latch;
+    Semaphore semaphore2;
+    CountDownLatch latch;
     int[] activity1;
     int[] activity2;
-    int[] activity3;
-    int sleep_min;
-    int sleep_max;
+    int sleep_min1;
+    int sleep_max1;
+    int sleep_min2;
+    int sleep_max2;
 
-    public ExecutionThread(Semaphore sem1, Semaphore sem2, CountDownLatch latch, int[] act1, int[] act2, int[] act3, int min, int max) {
-        this.semaphore1 = sem1;
-        this.semaphore2 = sem2;
+    public ExecutionThread(Semaphore s1, Semaphore s2, CountDownLatch latch, int[] activity1, int[] activity2, int min1, int max1, int min2, int max2) {
+        this.semaphore1 = s1;
+        this.semaphore2 = s2;
         this.latch = latch;
-        this.activity1 = act1;
-        this.activity2 = act2;
-        this.activity3 = act3;
-        this.sleep_min = min;
-        this.sleep_max = max;
+        this.activity1 = activity1;
+        this.activity2 = activity2;
+        this.sleep_min1 = min1;
+        this.sleep_max1 = max1;
+        this.sleep_min2 = min2;
+        this.sleep_max2 = max2;
     }
 
     public void run() {
-        System.out.println(this.getName() + " - State 1");
+        while(true) {
+            System.out.println(this.getName() + " - State 1");
 
-        // P9 Semaphore
-        try {
-            semaphore1.tryAcquire();
-        }
-        catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println(this.getName() + " - State 2");
-        int k = (int) Math.round(Math.random()*(activity1[1]- activity1[0]) + activity1[0]);
-        for (int i = 0; i < k * 100000; i++) {
-            i++;
-            i--;
-        }
-        try {
-            Thread.sleep(Math.round(Math.random() * (sleep_max - sleep_min)+ sleep_min) * 500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        semaphore1.release();
+            // P9 Semaphore
+            try {
+                semaphore1.acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(this.getName() + " - State 2");
+            int k = (int) Math.round(Math.random()*(activity1[1]- activity1[0]) + activity1[0]);
+            for (int i = 0; i < k * 100000; i++) {
+                i++;
+                i--;
+            }
+            try {
+                Thread.sleep(Math.round(Math.random() * (sleep_max1 - sleep_min1)+ sleep_min1) * 500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            semaphore1.release();
 
-        // P11 Semaphore
-        semaphore2.tryAcquire();
-        System.out.println(this.getName() + " - State 3");
-        k = (int) Math.round(Math.random()*(activity2[1]- activity2[0]) + activity2[0]);
-        for (int i = 0; i < k * 100000; i++) {
-            i++;
-            i--;
-        }
-        semaphore2.release();
+            // P11 Semaphore
+            try {
+                semaphore2.acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(this.getName() + " - State 3");
+            k = (int) Math.round(Math.random()*(activity2[1]- activity2[0]) + activity2[0]);
+            for (int i = 0; i < k * 100000; i++) {
+                i++;
+                i--;
+            }
+            try {
+                Thread.sleep(Math.round(Math.random() * (sleep_max2 - sleep_min2)+ sleep_min2) * 500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            semaphore2.release();
 
-        k = (int) Math.round(Math.random()*(activity3[1]- activity3[0]) + activity3[0]);
-        for (int i = 0; i < k * 100000; i++) {
-            i++;
-            i--;
-        }
+            System.out.println(this.getName() + " - State 4");
 
-        System.out.println(this.getName() + " - State 4");
-        // T7 CountDownLatch
-        try{
-            latch.countDown();
-            latch.await();
-        } catch (InterruptedException  e)
-        {
-            e.printStackTrace();
+            // T7 CountDownLatch
+            try{
+                latch.countDown();
+                latch.await();
+            } catch (InterruptedException  e)
+            {
+                e.printStackTrace();
+            }
+            System.out.println(this.getName() + " - END");
         }
     }
 }
--- src/LabExam/Q2/Main.java
+++ src/LabExam/Q2/Main.java
@@ -11,17 +11,22 @@ public class Main {
 
         int[] activity1 = {3, 6};
         int[] activity2 = {4, 5};
-        int[] activity3 = {2, 4};
-        int sleep1 = 3;
-        int[] activity4 = {2, 5};
-        int[] activity5 = {2, 6};
-        int[] activity6 = {3, 5};
-        int sleep2 = 2;
+        int sleep_min1 = 3;
+        int sleep_max1 = 3;
+        int sleep_min2 = 2;
+        int sleep_max2 = 4;
 
-        new ExecutionThread(s1, s2, latch, activity1, activity2, activity3, sleep1, sleep1).start();
-        new ExecutionThread(s1, s2, latch, activity4, activity5, activity6, sleep2, sleep2).start();
+        int[] activity3 = {2, 5};
+        int[] activity4 = {2, 6};
+        int sleep_min3 = 2;
+        int sleep_max3 = 2;
+        int sleep_min4 = 3;
+        int sleep_max4 = 5;
+
+        new ExecutionThread(s1, s2, latch, activity1, activity2, sleep_min1, sleep_max1, sleep_min2, sleep_max2).start();
+        new ExecutionThread(s1, s2, latch, activity3, activity4, sleep_min3, sleep_max3, sleep_min4, sleep_max4).start();
+
         latch.countDown();
-
         try {
             latch.await();
         } catch (InterruptedException e) {
